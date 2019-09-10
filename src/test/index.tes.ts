175f2f1e58c855426cc8633908f57e5c307f1c7f
Sodimu Segun <princesegzy01@yahoo.com>
2019-09-10 22:26:14 +0100

    setup test rice

---
+++ src/test/index.tes.ts
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const expect = require('chai').expect
+const request = require('supertest');
+const app = require('../app')
+
+describe('Unit testing the /home route', function() {
+
+});
