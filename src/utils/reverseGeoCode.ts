f7cd3acd038be2a0b5275ecd35ba5ecc503e63d6
Sodimu Segun <princesegzy01@yahoo.com>
2019-09-10 18:02:11 +0100

    tslint reverseGeoCode

--- src/utils/reverseGeoCode.ts
+++ src/utils/reverseGeoCode.ts
@@ -1,12 +1,17 @@
 import { createClient } from "@google/maps";
 import _ from "lodash";
 
+// create a googleMap client object
 const googleMapsClient = createClient({
 	key: "AIzaSyBI7Rad8slJbP0_wbjmbClKOMFMSdlbj0I",
 });
 
+// getlocation function takes in an address or a zip as inpuy
+// then return a cordianates with a postcode as output
 const getLocation = (addr: string) => {
 	return new Promise((resolve, reject) => {
+
+		// use the geocode methode to get cordinates
 		googleMapsClient.geocode(
 			{
 				address: addr,
@@ -16,21 +21,30 @@ const getLocation = (addr: string) => {
 					reject(err);
 				}
 
+				// store the addres component object
+				// to retrieve the postal code
 				const addressComponentArray =
 					response.json.results[0].address_components;
 
 				let postalCode = "";
+
+				// loop through the array to get the postcode
+				// from the nodes
 				addressComponentArray.forEach((address) => {
 					if (address.types[0] === "postal_code") {
 						postalCode = address.long_name;
 					}
 				});
 
+				// construct a result object to be retun to the caller which includes
+				// 1. cordinates of a given zip or address
+				// 2. postal codes of the cordinares
 				const result = {
 					cordinates: response.json.results[0].geometry.location,
 					post_code: postalCode,
 				};
 
+				// return the result as a promise
 				resolve(result);
 			},
 		);
--- utils/reverseGeoCode.js
+++ utils/reverseGeoCode.js
@@ -1,28 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var maps_1 = require("@google/maps");
+// create a googleMap client object
 var googleMapsClient = maps_1.createClient({
     key: "AIzaSyBI7Rad8slJbP0_wbjmbClKOMFMSdlbj0I",
 });
+// getlocation function takes in an address or a zip as inpuy
+// then return a cordianates with a postcode as output
 var getLocation = function (addr) {
     return new Promise(function (resolve, reject) {
+        // use the geocode methode to get cordinates
         googleMapsClient.geocode({
             address: addr,
         }, function (err, response) {
             if (err) {
                 reject(err);
             }
+            // store the addres component object
+            // to retrieve the postal code
             var addressComponentArray = response.json.results[0].address_components;
             var postalCode = "";
+            // loop through the array to get the postcode
+            // from the nodes
             addressComponentArray.forEach(function (address) {
                 if (address.types[0] === "postal_code") {
                     postalCode = address.long_name;
                 }
             });
+            // construct a result object to be retun to the caller which includes
+            // 1. cordinates of a given zip or address
+            // 2. postal codes of the cordinares
             var result = {
                 cordinates: response.json.results[0].geometry.location,
                 post_code: postalCode,
             };
+            // return the result as a promise
             resolve(result);
         });
     });
