36e7ee1c0a9e54618752d22ffd89fa26041aa2d0
Sodimu Segun <princesegzy01@yahoo.com>
2019-09-10 18:06:47 +0100

    lint distance calculator

--- src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import express  from 'express';
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
